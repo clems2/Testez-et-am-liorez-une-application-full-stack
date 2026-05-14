ed3ec5db26f35118cb4b27c827b08f6d5330f535
@clemcir <clementcirou22@gmail.com>
2026-05-14 19:08:46 +0200

    feat : prepared exception which will be catched by the handler

---
+++ back/src/main/java/com/openclassrooms/starterjwt/exception/UnauthorizedException.java
@@ -0,0 +1,11 @@
+package com.openclassrooms.starterjwt.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException() {
+        super();
+    }
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
